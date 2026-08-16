8064a41a9967ef472c4a75888d7372dfda927a5e
Uellei <wessmarcelo@gmail.com>
2026-08-16 14:34:35 -0300

    chore: update game to render grass

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import pygame
 
 from camera import Camera
 from player import Player
+from rendering.grass import GrassManager
 
 
 def main() -> None:
@@ -14,6 +15,19 @@ def main() -> None:
     
     player = Player((600, 300))
     camera = Camera((1280, 720))
+    grass = GrassManager(
+        "../assets/grass",
+        tile_size=15,
+        place_range=[0,1]
+    )
+    
+    for y in range(10):
+        for x in range(10):
+            grass.place_tile(
+                (x, y),
+                density=10,
+                grass_options=[0]
+            )
     
     running = True
     
@@ -26,6 +40,10 @@ def main() -> None:
             if event.type == pygame.QUIT:
                 running = False
         
+        # Player position -> Where the player is in the world
+        # Camera position -> Where starts the region on the world that is being displayed on the screen
+        # Screen position = player position - camera position -> Transform the player position from world coordinates to screen coordinates
+        
         # Update playwer position based on input and delta time
         player.update(dt)
         
@@ -33,6 +51,12 @@ def main() -> None:
         camera.update(player.position)
                 
         screen.fill((30, 100, 30))
+        
+        grass.update_render(
+            screen,
+            dt,
+            offset=camera.position
+        )
         
         # Draw the player on the screen
         player.draw(screen, camera)
